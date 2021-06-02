a8057510a722f01012661f9a0e2af4f7d2cb505f
Merlin Dugot <merlin.dugot@gmail.com>
2021-06-02 10:59:48 +0900

    use cuda if available

--- src/config.py
+++ src/config.py
@@ -11,7 +11,7 @@ class Config:
     noise = 100
     generator_hidden_layer = 4096
     discriminator_hidden_layer = 256
-    device = "cuda" torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     batch_size = 32
     gen_learning_rate = 0.0001
     dis_learning_rate = 0.00001
